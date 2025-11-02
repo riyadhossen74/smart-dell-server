8740a34ed6a67afd6643cadfa46a605ce7052766
riyadhossen74 <riyadhossen696969@gmail.com>
2025-11-02 20:13:23 +0600

    some comment

--- index.js
+++ index.js
@@ -39,17 +39,17 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/products", async (req, res) => {
-      const cursor = productCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-    app.get("/products/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id)}
-      const result = await productCollection.findOne(query);
-      res.send(result);
-    });
+    // app.get("/products", async (req, res) => {
+    //   const cursor = productCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+    // app.get("/products/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id)}
+    //   const result = await productCollection.findOne(query);
+    //   res.send(result);
+    // });
 
 
     app.patch("/products/:id", async (req, res) => {
